2b08e3f92529eb2ae67693b1df5ede74e0d8b4c5
Tran Hien Vinh <tranvinh141203@gmail.com>
2025-04-11 12:22:05 +0700

    Chức năng cập nhật thông tin người dùng

--- Chat_Web_FE/src/hooks/useUser.js
+++ Chat_Web_FE/src/hooks/useUser.js
@@ -1,10 +1,14 @@
-import { useQuery } from "@tanstack/react-query";
-import { getCurrentUserService } from "../services/UserService";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+  getCurrentUserService,
+  updateUserService,
+} from "../services/UserService";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 
 const useUser = () => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const {
     data: currentUser,
@@ -27,11 +31,27 @@ const useUser = () => {
     }
   }, [isError, error, navigate]);
 
+  // Mutation để cập nhật user
+  const {
+    mutate: updateUser,
+    isLoading: isUpdating,
+    isError: isUpdateError,
+    error: updateError,
+  } = useMutation({
+    mutationFn: updateUserService,
+    onSuccess: () => {
+      queryClient.invalidateQueries(["currentUser"]);
+    },
+  });
   return {
     currentUser,
     isLoading,
     isError,
     error,
+    updateUser,
+    isUpdating,
+    isUpdateError,
+    updateError,
   };
 };
 
